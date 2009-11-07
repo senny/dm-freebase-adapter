bd161dbf7303173e9e9d8f171866fbfc7ec84078
Yves Senn <yves.senn@gmail.com>
2009-11-07 17:51:28 +0100

    basic support for associations and added MQL operators

--- lib/freebase_adapter.rb
+++ lib/freebase_adapter.rb
@@ -18,19 +18,88 @@ module DataMapper
         metaweb_query = {}
         fields = query.fields
         query.conditions.each do |condition|
-          metaweb_query[condition.subject.name] = condition.value
-        end
-        looking_for = fields.reject {|field| metaweb_query.include?(field.name)}
-        looking_for.each do |field|
-          metaweb_query[field.name] = []
+          metaweb_query[build_metaweb_condition_key(condition)] = build_metaweb_condition(condition)
+          looking_for = fields.reject {|field| metaweb_query.include?(field.name)}
+          looking_for.each do |field|
+            metaweb_query[field.name] = [{}]
+          end
         end
         metaweb_query["type"] = query.model.storage_name
-        metaweb_read(metaweb_query)["result"]
+        result = metaweb_read(metaweb_query)["result"]
+        convert_metaweb_result(result)
       end
 
       private
 
+      def build_metaweb_condition_key(condition)
+        puts condition.class
+        case condition
+        when DataMapper::Query::Conditions::LikeComparison
+          "#{condition.subject.name}~="
+        when DataMapper::Query::Conditions::InclusionComparison
+          "#{condition.subject.name}|="
+        when DataMapper::Query::Conditions::LessThanComparison
+          "#{condition.subject.name}<"
+        when DataMapper::Query::Conditions::GreaterThanComparison
+          "#{condition.subject.name}>"
+        when DataMapper::Query::Conditions::LessThanOrEqualToComparison
+          "#{condition.subject.name}<="
+        when DataMapper::Query::Conditions::GreaterThanOrEqualToComparison
+          "#{condition.subject.name}>="
+        when DataMapper::Query::Conditions::NotOperation
+          "#{condition.subject.name}!="
+        else
+          condition.subject.name
+        end
+      end
+
+      def build_metaweb_condition(condition)
+        case condition.subject
+        when DataMapper::Associations::ManyToOne::Relationship
+          {:id => condition.value.id}
+          break
+        else
+          condition.value
+        end
+      end
+
+      def convert_metaweb_result(results)
+        # puts result.inspect
+        results.each do |element|
+          element.each do |key, value|
+            element[key] = extract_value(value)
+          end
+        end
+      end
+
+      def extract_value(property)
+        case property
+        when Array
+          values = property.collect {|element| extract_value(element)}
+          values.size == 1 ? values.first : values
+        when Hash
+          if property.has_key?("type")
+            extract_typed_value(property)
+          else
+            property
+          end
+        else
+          property
+        end
+      end
+
+      def extract_typed_value(property)
+        case property["type"]
+        when "/type/text", "/type/id"
+          property["value"]
+        else
+          property["name"]
+        end
+      end
+
       def metaweb_read(query)
+        puts JSON.dump(query)
+        puts ""
         metaweb_query = "?query={\"query\": [#{JSON.dump(query)}]}"
         url = "#{query_url}#{URI.escape(metaweb_query)}"
         response = Net::HTTP.get_response(URI.parse(url))
@@ -39,6 +108,7 @@ module DataMapper
         result = JSON.parse(data)
 
         if result['code'] == '/api/status/error'
+          puts result.inspect
           raise "web service error #{url}"
         end
         return result
--- spec/freebase_adapter_spec.rb
+++ spec/freebase_adapter_spec.rb
@@ -3,18 +3,62 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 class Artist
   include DataMapper::Resource
 
-  property :id, String, :key => true
-  property :album, String
-  property :genre, String
-  property :label, String
-
   storage_names[:default] = '/music/artist'
+
+  property :id, String, :key => true
+  property :guid, String
+  property :name, String
+  property :genre, String
+
+  has n, :albums, :child_key => [:artist]
+end
+
+class Album
+  include DataMapper::Resource
+
+  storage_names[:default] = '/music/album'
+
+  property :id, String, :key => true
+  property :name, String
+  property :track, String
+
+  belongs_to :artist, :child_key => [:artist]
 end
 
 describe "FreebaseAdapter" do
 
-  it "should" do
-    puts Artist.get('/en/paul_kalkbrenner').inspect
+  it "should fetch the given properties" do
+    artist = Artist.get('/en/paul_kalkbrenner')
+    artist.name.should == "Paul Kalkbrenner"
+    artist.genre.should_not be_empty
+  end
+
+  it "should fetch associations on demand" do
+    artist = Artist.get('/en/apparat')
+    artist.albums.size.should > 0
+    titles = artist.albums.collect(&:name)
+    ["Silizium EP", "Walls"].each do |title|
+      titles.should include(title)
+    end
+  end
+
+  it "should work with inclusion (in) queries" do
+    results = Album.all(:name => ["Berlin Calling", "Balance 005: James Holden"])
+    results.size.should == 2
+    results[0].name.should == "Balance 005: James Holden"
+    results[1].name.should == "Berlin Calling"
+  end
+
+  it "should work with like (regexp match) queries" do
+    results = Album.all(:name.like => "Balance 00*")
+    results.size.should > 0
+    titles = results.collect(&:name)
+    titles.each {|title| title.should =~ /^Balance 00.*$/}
+  end
+
+  it "should work with greather than queries" do
+    results = Artist.first(:name => "Paul Kalkbrenner")
+    puts results.inspect
   end
 
 end
